a2465692bc3b1a3fcd650f538236c5974bd55708
stefan <top_scot2@yahoo.com.au>
2021-04-06 15:36:12 +0930

    unit1 q2 v1

---
+++ unit1-solution2.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+mkdir -m 775 unit1exercise2
+chown student:student unit1exercise2
+cd unit1exercise2
+
+mkdir -m 603 verrennkeit 
+chown news:floppy verrennkeit 
+
+mkdir -m 653 aufgewarfs 
+chown mail:floppy aufgewarfs
+
+mkdir -m 610 aushundheit
+chown news:student aushundheit
+
+mkdir -m 353 besprachtest
+chown proxy:fax besprachtest
+
+mkdir -m 073 enpflums
+chown news:fax enpflums
+
+mkdir -m 012 enpflumen
+chown news:news enpflumen
+cd enpflumen
+mkdir -m 243 beklettte
+chown mail:student beklettte
+mkdir -m 173 aufgesitzse
+chown news:cdrom aufgesitzse
+mkdir -m 227 aufgesitzse/zerraucher
+chown nobody:tape aufgesitzse/zerraucher
+cd ..
+
+mkdir -m 507 angetritter
+chown news:tape angetritter
+cd angetritter
+mkdir -m 034 beschmeckung
+chown lp:audio beschmeckung
+cd beschmeckung 
+mkdir -m 323 ausgesprachs
+chown news:student ausgesprachs
+mkdir -m 420 aufkletts
+chown proxy:floppy aufkletts
+mkdir -m 721 ausrauchheit
+chown nobody:cdrom ausrauchheit
+cd ..
+mkdir -m 116 antrittung
+chown mail:student antrittung
+mkdir -m 444 antrittung/ankaeser
+chown games:dip antrittung/ankaeser
+cd ..
+
+mkdir -m 711 verklettt
+chown proxy:tape verklettt
+cd verklettt
+mkdir -m 725 aufwarfheit
+chown proxy:voice aufwarfheit
+cd aufwarfheit
+mkdir -m 337 enwarfst
+chown games:tape enwarfst
+mkdir -m 341 bestehs
+chown news:fax bestehs
+
+
+
+
+
