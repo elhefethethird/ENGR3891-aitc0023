df5dde71d5186732ffbbfad66095f79348f15e53
stefan <top_scot2@yahoo.com.au>
2021-04-06 12:52:50 +0930

    q1 unit1

---
+++ unit1-solution1.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+
+mkdir -m 775 unit1exercise1
+chown student:student unit1exercise1
+cd unit1exercise1
+
+mkdir -m 400 verrabarbst
+chown student:student verrabarbst
+
+mkdir -m 660 enhundst
+chown student:student enhundst
+
+mkdir -m 003 bepflumheit
+chown student:student bepflumheit
+
+mkdir -m 072 zersinnse
+chown student:student zersinnse
+
+mkdir -m 316 besprachst
+chown student:student besprachst
+
+
+mkdir -m 774 verkaesen
+chown student:student verkaesen
+
+mkdir -m 507 verkaesen/aufwarftest
+chown student:student verkaesen/aufwarftest
+
+mkdir -m 205 verkaesen/aufwarftest/aufgerennung
+chown student:student verkaesen/aufwarftest/aufgerennung
+
+
+
+mkdir -m 500 auswitzer
+chown student:student auswitzer
+cd auswitzer
+
+mkdir -m 762 aufgerabarbt
+chown student:student aufgerabarbt
+
+mkdir -m 002 angewarfen
+chown student:student angewarfen
+
+mkdir -m 446 angetraust
+chown student:student angetraust
+
+mkdir -m 336 aufgerabarbt/befahrt
+chown student:student aufgerabarbt/befahrt
+
+mkdir -m 162 aufgerabarbt/einkrauen
+chown student:student aufgerabarbt/einkrauen
+
+mkdir -m 002 angewarfen/aufgesetzheit
+chown student:student angewarfen/aufgesetzheit
+
+mkdir -m 231 angetraust/auspflumse
+chown student:student angetraust/auspflumse
+cd ..
+
+
+
+
+
+
+
+mkdir -m 626 zertritter
+chown student:student zertritter
+cd zertritter
+
+mkdir -m 367 aufklettheit
+chown student:student aufklettheit
+cd aufklettheit
+
+mkdir -m 212 angerabarbheit
+chown student:student angerabarbheit
+
+mkdir -m 572 angekrauung
+chown student:student angekrauung
+
+
+
+
