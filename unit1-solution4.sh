f0719f6e9b1e1443a8f3b11489ca488718d01bd5
stefan <top_scot2@yahoo.com.au>
2021-04-08 12:30:32 +0930

    u1 q4-v1

---
+++ unit1-solution4.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+mkdir -m 775 unit1exercise4
+chown student:student unit1exercise4
+cd unit1exercise4
+
+mkdir -m 320 begehs 
+chown student:fax begehs
+
+mkdir -m 457 aufwitzung 
+chown student:proxy aufwitzung
+
+mkdir -m 206 einstehst 
+chown student:tape einstehst 
+
+mkdir -m 2650 gelaufkeit 
+chown student:student gelaufkeit
+
+mkdir -m 2127 verfahrtest 
+chown student:audio verfahrtest
+cd verfahrtest
+mkdir -m 2761 angegeher 
+chown student:cdrom angegeher 
+mkdir -m 2542 angegeher/angehalttete 
+chown student:voice angegeher/angehalttete
+cd ..
+
+mkdir -m 147 verfahrs 
+chown student:dip verfahrs
+mkdir -m 2657 verfahrs/bewitzse 
+chown student:voice verfahrs/bewitzse
+
+mkdir -m 2523 einwitzse 
+chown student:voice einwitzse
+cd einwitzse
+mkdir -m 257 befahrtete 
+chmod g-s befahrtete 
+chown student:tape befahrtete 
+cd befahrtete
+mkdir -m 603 angesetztest 
+chmod g-s angesetztest 
+chown student:floppy angesetztest
+mkdir -m 707 aufstehen 
+chmod g-s aufstehen
+chown student:dip aufstehen
+cd ..
+mkdir -m 047 verklettung 
+chmod g-s verklettung
+chown student:cdrom verklettung 
+mkdir -m 2736 verklettung/belaufung 
+chown student:proxy verklettung/belaufung
+cd ..
+
+mkdir -m 2111 auffahren
+chown student:news auffahren
+cd auffahren 
+mkdir -m 0230 gekaest
+chmod g-s gekaest
+chown student:student gekaest
+cd gekaest
+mkdir -m 2376 zerstehung
+chown student:mail zerstehung
+mkdir -m 2135 auftrittung
+chown student:tape auftrittung
+mkdir -m 2102 enrauchkeit
+chown student:floppy enrauchkeit
+
+
+
+
+
+
+
+
