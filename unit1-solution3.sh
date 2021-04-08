b1bb1efb4cfe0a52c2263e26a3c33b37c00f48d9
stefan <top_scot2@yahoo.com.au>
2021-04-08 11:12:18 +0930

    u1 q3-v1

---
+++ unit1-solution3.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+mkdir -m 775 unit1exercise3
+chown student:student unit1exercise3
+cd unit1exercise3
+
+
+mkdir -m 764 anwarftest 
+chown proxy:dip anwarftest
+
+mkdir -m 4650 einrauchen 
+chown uucp:proxy einrauchen
+
+mkdir -m 4354 angetrittst 
+chown proxy:mail angetrittst
+cd angetrittst
+mkdir -m 4727 versprachst 
+chown mail:proxy versprachst
+mkdir -m 620 versprachst/gepflumse 
+chown mail:fax versprachst/gepflumse
+cd ..
+
+mkdir -m 405 einrabarber 
+chown student:dip einrabarber
+
+mkdir -m 230 aufgetritttete
+chown news:mail aufgetritttete
+cd aufgetritttete
+mkdir -m 4016 aufgefahrtete 
+chown games:voice aufgefahrtete 
+mkdir -m 251 aufgefahrtete/aufgewitzung 
+chown news:dip aufgefahrtete/aufgewitzung 
+mkdir -m 4024 aufgefahrtete/ausgehaltheit 
+chown proxy:news aufgefahrtete/ausgehaltheit
+mkdir -m 4756 aufgefahrtete/aufrennt 
+chown news:uucp aufgefahrtete/aufrennt
+cd ..
+
+mkdir -m 4071 auswitzte 
+chown mail:audio auswitzte 
+mkdir -m 641 auswitzte/angeschmecktest
+chown student:news auswitzte/angeschmecktest
+
+mkdir -m 4337 zersetzt 
+chown uucp:dip zersetzt
+mkdir -m 4245 zersetzt/ausgekrauen 
+chown mail:uucp zersetzt/ausgekrauen 
+
+
+mkdir -m 460 zerrabarbt 
+chown uucp:dip zerrabarbt
+cd zerrabarbt
+mkdir -m 432 angerabarbs 
+chown mail:mail angerabarbs
+cd angerabarbs
+mkdir -m 4723 bestehkeit 
+chown games:student bestehkeit
+mkdir -m 4274 bekraus 
+chown nobody:floppy bekraus 
+mkdir -m 732 anfahren 
+chown mail:student anfahren 
+
+
+
+
+
